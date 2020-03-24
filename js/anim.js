6aca9045b0e4f0e573d0666727e46c9b9527734e
Paelion <aelion.pauline1@gmail.com>
2020-03-24 18:36:15 +0100

    add pyramide 3D

--- js/anim.js
+++ js/anim.js
@@ -26,14 +26,50 @@ window.addEventListener('resize', function () {
 
 });
 
-var geometry = new THREE.ConeBufferGeometry(1, 1, 4, 4,true );
+/*var geometry = new THREE.ConeBufferGeometry(1, 1, 4, 4,true );
 
 var material = new THREE.MeshBasicMaterial({color: 0xFFFFFF, wireframe: true});
 
 var cone = new THREE.Mesh(geometry, material);
-scene.add(cone);
+scene.add(cone);*/
 
-camera.position.z = 3;
+
+
+var geometry = new THREE.BoxGeometry(1,1,1);
+var material = new THREE.MeshBasicMaterial({color : 0x00ff00});
+
+camera.position.z = 5;
+
+var keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
+keyLight.position.set(-100, 0, 100);
+
+
+var fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), .75);
+fillLight.position.set(100, 0, 100);
+
+var backLight = new THREE.DirectionalLight(new THREE.Color(0x404040, 1.0));
+backLight.position.set(100, 0, -100).normalize();
+
+var light = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add( light );
+
+scene.add(keyLight);
+scene.add(fillLight);
+scene.add(backLight);
+
+
+
+var objLoader = new THREE.OBJLoader();
+objLoader.load('louvre.obj', function (object) {
+
+    scene.add(object);
+});
+
+
+
+
+
+
 
 
 
